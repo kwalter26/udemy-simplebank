cbe5a1647a143dac9f57335de236a1661bf5cf03
Kyle Walter <walter.kl26@gmail.com>
2023-06-01 23:45:39 -0500

    Fixing code duplication

--- api/health_check_test.go
+++ api/health_check_test.go
@@ -10,16 +10,17 @@ import (
 	"testing"
 )
 
-func TestGetHealthz(t *testing.T) {
+func TestGetHealthAndReady(t *testing.T) {
 
 	testCases := []struct {
 		name          string
+		url           string
 		buildStubs    func(store *mockdb.MockStore)
 		checkResponse func(recorder *httptest.ResponseRecorder)
 	}{
 		{
 			name: "OK",
-
+			url:  "/healthz",
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().Ping(gomock.Any()).Times(1).Return(nil)
 			},
@@ -30,6 +31,29 @@ func TestGetHealthz(t *testing.T) {
 		},
 		{
 			name: "DB Error",
+			url:  "/healthz",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().Ping(gomock.Any()).Times(1).Return(errors.New("db error"))
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+				require.Equal(t, `{"error":"db error"}`, recorder.Body.String())
+			},
+		},
+		{
+			name: "OK",
+			url:  "/readyz",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().Ping(gomock.Any()).Times(1).Return(nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				require.Equal(t, `{"status":"Ok"}`, recorder.Body.String())
+			},
+		},
+		{
+			name: "DB Error",
+			url:  "/readyz",
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().Ping(gomock.Any()).Times(1).Return(errors.New("db error"))
 			},
@@ -53,56 +77,6 @@ func TestGetHealthz(t *testing.T) {
 			recorder := httptest.NewRecorder()
 
 			request, err := http.NewRequest(http.MethodGet, "/healthz", nil)
-			require.NoError(t, err)
-
-			server.router.ServeHTTP(recorder, request)
-			tc.checkResponse(recorder)
-		})
-	}
-}
-
-// Test the GeReadyz handler
-func TestGetReady(t *testing.T) {
-	testCases := []struct {
-		name          string
-		buildStubs    func(store *mockdb.MockStore)
-		checkResponse func(recorder *httptest.ResponseRecorder)
-	}{
-		{
-			name: "OK",
-			buildStubs: func(store *mockdb.MockStore) {
-				store.EXPECT().Ping(gomock.Any()).Times(1).Return(nil)
-			},
-			checkResponse: func(recorder *httptest.ResponseRecorder) {
-				require.Equal(t, http.StatusOK, recorder.Code)
-				require.Equal(t, `{"status":"Ok"}`, recorder.Body.String())
-			},
-		},
-		{
-			name: "DB Error",
-			buildStubs: func(store *mockdb.MockStore) {
-				store.EXPECT().Ping(gomock.Any()).Times(1).Return(errors.New("db error"))
-			},
-			checkResponse: func(recorder *httptest.ResponseRecorder) {
-				require.Equal(t, http.StatusInternalServerError, recorder.Code)
-				require.Equal(t, `{"error":"db error"}`, recorder.Body.String())
-			},
-		},
-	}
-
-	for i := range testCases {
-		tc := testCases[i]
-		t.Run(tc.name, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-
-			store := mockdb.NewMockStore(ctrl)
-			tc.buildStubs(store)
-
-			server := newTestServer(t, store)
-			recorder := httptest.NewRecorder()
-
-			request, err := http.NewRequest(http.MethodGet, "/readyz", nil)
 			require.NoError(t, err)
 
 			server.router.ServeHTTP(recorder, request)
