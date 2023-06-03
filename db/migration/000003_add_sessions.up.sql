f8772cbb6e89a094b63261ef4133f609068eb5df
Kyle Walter <walter.kl26@gmail.com>
2023-06-02 22:30:22 -0500

    Fixing migration Adding local environment variable for testing Adding tests for session

--- db/migration/000003_add_sessions.up.sql
+++ db/migration/000003_add_sessions.up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE "sessions"
 (
     "id"            uuid PRIMARY KEY,
-    "username"      varchar   NOT NULL,
-    "refresh_token" varchar   NOT NULL,
-    "user_agent"    varchar   NOT NULL,
-    "client_ip"     varchar   NOT NULL,
-    "is_blocked"    boolean   NOT NULL DEFAULT false,
-    "expires_at"    timestamp NOT NULL,
-    "created_at"    timestamp NOT NULL DEFAULT now()
+    "username"      varchar     NOT NULL,
+    "refresh_token" varchar     NOT NULL,
+    "user_agent"    varchar     NOT NULL,
+    "client_ip"     varchar     NOT NULL,
+    "is_blocked"    boolean     NOT NULL DEFAULT false,
+    "expires_at"    timestamptz NOT NULL,
+    "created_at"    timestamptz NOT NULL DEFAULT now()
 );
 
 ALTER TABLE "sessions"
